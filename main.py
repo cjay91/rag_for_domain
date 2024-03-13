9615a6cb5111ef7bc3d20eed96e01d3659d72252
chathura <chathura.jayathilake@gmail.com>
2024-03-13 09:30:35 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+)
+from langchain.prompts.chat import (
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.vectorstores import Chroma
+
+load_dotenv()
+
+template: str = """/
+    You are a customer support specialist /
+    question: {question}. You assist users with general inquiries based on {context} /
+    and  technical issues. /
+    """
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_message_prompt = HumanMessagePromptTemplate.from_template(
+    input_variables=["question", "context"],
+    template="{question}",
+)
+chat_prompt_template = ChatPromptTemplate.from_messages(
+    [system_message_prompt, human_message_prompt]
+)
+model = ChatOpenAI()
