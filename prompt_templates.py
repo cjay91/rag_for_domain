22b10657ff9f013622e4553d18fee8a7eab9a1f7
chathura <chathura.jayathilake@gmail.com>
2024-03-15 09:55:24 +0530

    Adding Prompt Templates

---
+++ prompt_templates.py
@@ -0,0 +1,19 @@
+from langchain.prompts.chat import (
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain.prompts import ChatPromptTemplate
+
+template: str = """/
+    You are a customer support specialist /
+    question: {question}. You assist users with general inquiries based on {context} /
+    and  technical issues. /
+    """
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_message_prompt = HumanMessagePromptTemplate.from_template(
+    input_variables=["question", "context"],
+    template="{question}",
+)
+chat_prompt_template = ChatPromptTemplate.from_messages(
+    [system_message_prompt, human_message_prompt]
+)
